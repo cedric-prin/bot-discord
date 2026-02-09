7dee4731f283c344cbe4a74f4e504928e9785e1b
Cédric Prin <prin.cedric.34@gmail.com>
2026-02-09 14:28:12 +0100

    Implement command and event handlers

--- bot/handlers/commandHandler.js
+++ bot/handlers/commandHandler.js
@@ -1,1 +1,37 @@
-// Handler des commandes
+const fs = require('fs');
+const path = require('path');
+const { Collection } = require('discord.js');
+const logger = require('../utils/logger');
+
+function loadCommands(dir, commands, errors) {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    for (const file of files) {
+        const fullPath = path.join(dir, file.name);
+        if (file.isDirectory()) {
+            loadCommands(fullPath, commands, errors);
+        } else if (file.isFile() && file.name.endsWith('.js')) {
+            try {
+                const command = require(fullPath);
+                if (!command.data || typeof command.execute !== 'function') {
+                    errors.push(`Commande invalide : ${fullPath}`);
+                    continue;
+                }
+                commands.set(command.data.name, command);
+            } catch (err) {
+                errors.push(`Erreur lors du chargement de ${fullPath} : ${err.message}`);
+            }
+        }
+    }
+}
+
+module.exports = (client) => {
+    client.commands = new Collection();
+    const commandsPath = path.join(__dirname, '../commands');
+    const errors = [];
+    loadCommands(commandsPath, client.commands, errors);
+    logger.info(`${client.commands.size} commandes chargées`);
+    if (errors.length > 0) {
+        errors.forEach(e => logger.error(e));
+        throw new Error('Certaines commandes sont invalides ou n\'ont pas pu être chargées.');
+    }
+};
--- bot/handlers/eventHandler.js
+++ bot/handlers/eventHandler.js
@@ -1,1 +1,28 @@
-// Handler des events
+const fs = require('fs');
+const path = require('path');
+const logger = require('../utils/logger');
+
+module.exports = (client) => {
+    const eventsPath = path.join(__dirname, '../events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    let loaded = 0;
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        try {
+            const event = require(filePath);
+            if (!event.name || typeof event.execute !== 'function') {
+                logger.error(`Événement invalide : ${file}`);
+                continue;
+            }
+            if (event.once) {
+                client.once(event.name, (...args) => event.execute(...args));
+            } else {
+                client.on(event.name, (...args) => event.execute(...args));
+            }
+            loaded++;
+        } catch (err) {
+            logger.error(`Erreur lors du chargement de l'événement ${file} : ${err.message}`);
+        }
+    }
+    logger.info(`${loaded} événements chargés`);
+};
