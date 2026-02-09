19e6a402651ae5f79244c18718f16d1112b38167
Aboubacar M'houmadi <aboubacarm279@gmail.com>
2026-02-09 14:41:55 +0100

    ajouts inde.js + logger.js

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,36 @@
 // Point d'entrée du bot Discord
+require('dotenv').config();
+
+const config = require('../../config/config');
+const client = require('./bot');
+const logger = require('./utils/logger');
+const loadCommands = require('./handlers/commandHandler');
+const loadEvents = require('./handlers/eventHandler');
+
+async function start() {
+  try {
+    logger.info('Démarrage du bot...');
+
+    // Charger les commandes et événements
+    loadCommands(client);
+    loadEvents(client);
+
+    // Connexion
+    await client.login(config.bot.token);
+  } catch (err) {
+    logger.error(`Erreur au démarrage : ${err.message}`);
+    process.exit(1);
+  }
+}
+
+// Gestion des erreurs non capturées
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+});
+
+process.on('uncaughtException', (err) => {
+  logger.error(`Uncaught Exception: ${err}`);
+  process.exit(1);
+});
+
+start();
