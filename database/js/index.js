868d6997991280d25f5213105b2ea642213e54ef
Cédric Prin <prin.cedric.34@gmail.com>
2026-02-09 16:58:06 +0100

    Add database init scripts

--- database/js/index.js
+++ database/js/index.js
@@ -1,1 +1,7 @@
 // Accès JS à la base de données
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/db.sqlite', (err) => {
+    if (err) console.error('Erreur de connexion à la base:', err.message);
+    else console.log('Connecté à la base SQLite.');
+});
+module.exports = db;
